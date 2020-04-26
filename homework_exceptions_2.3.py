190131ac17a2c2aea7c99085c5b20bbab6a61c90
Andrey_Gorbi <agorbi@ya.ru>
2020-04-27 02:28:20 +0300

    fixed work

--- homework_exceptions_2.3.py
+++ homework_exceptions_2.3.py
@@ -9,46 +9,45 @@ def main():
 
     inp_string = list(filter(None, inp))
 
-    if len(inp_string) > 3:
-        print('\n Вы ввели слишком много аргументов')
-
     action = 0
 
     try:
-        for i in inp_string:
-            if i == '-':
-                action = int(inp_string[1]) - int(inp_string[2])
-                print(f'\n Результат равен {action}')
-                break
-            elif i == '+':
-                action = int(inp_string[1]) + int(inp_string[2])
-                print(f'\n Результат равен {action}')
-                break
-            elif i == '*':
-                action = int(inp_string[1]) * int(inp_string[2])
-                print(f'\n Результат равен {action}')
-                break
-            elif i == '/':
-                action = int(inp_string[1]) / int(inp_string[2])
-                print(f'\n Результат равен {action}')
-                break
-
-        assert inp_string[0] in ['+', '-', '*', '/'], 'Ваша операция не находится в списке допустимых'
+        assert inp_string[0] in ['+', '-', '*', '/']
+    except AssertionError:
+        print('\n Ваша операция не находится в списке допустимых')
+    try:
         if int(inp_string[1]) < 0 or int(inp_string[2]) < 0:
-            print('Вы ввели отрицательное значение\n ')
+            print('Вы ввели отрицательное число')
             main()
         if not isinstance(inp_string[1:2], int):
             a = int(inp_string[1])
             b = int(inp_string[2])
-        c = a / b
-    except AssertionError:
-        print('\n Ваша операция не находится в списке допустимых')
+            c = a / b        
     except ZeroDivisionError:
         print('\n Вы пытаетесь поделить на ноль')
     except ValueError:
         print('\n Вы ввели не числовые значения')
     except IndexError:
         print('\n Вы ввели не достаточно аргументов')
+    if len(inp_string) > 3:
+        print('\n Вы ввели избыточное количество аргументов')
+
+    for i in inp_string:
+        if i == '-':
+            action = int(inp_string[1]) - int(inp_string[2])
+            print(f'\n Результат равен {action}')
+            break
+        elif i == '+':
+            action = int(inp_string[1]) + int(inp_string[2])
+            print(f'\n Результат равен {action}')
+            break
+        elif i == '*':
+            action = int(inp_string[1]) * int(inp_string[2])
+            print(f'\n Результат равен {action}')
+            break
+        elif i == '/':
+            action = int(inp_string[1]) / int(inp_string[2])
+            print(f'\n Результат равен {action}')
+            break
     
-   
 main()
